10996880538c4cff0665d612399ac575933d5e6c
TeYuan Wang <kamewang@google.com>
2025-01-07 16:47:25 +0000

    thermal: Low power shutdown policy bringup
    Bug: 363336093
    Bug: 361454856
    Bug: 363070211
    Test: emul_temp battery and VIRTUAL-SKIN for idle, camera use cases
    and check thermal log.
    
    Flag: EXEMPT config change
    Change-Id: I81f19bb87a51879ddffebec3df45452f0b681891

--- device-comet.mk
+++ device-comet.mk
@@ -197,6 +197,7 @@ PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_backup_comet.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_backup.json \
 	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/vt_estimation_model_comet.tflite:$(TARGET_COPY_OUT_VENDOR)/etc/vt_estimation_model.tflite \
 	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/vt_speaker_estimation_model_comet.tflite:$(TARGET_COPY_OUT_VENDOR)/etc/vt_speaker_estimation_model.tflite \
+        $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_lpm_comet.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_lpm.json \
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 	PRODUCT_COPY_FILES += \
