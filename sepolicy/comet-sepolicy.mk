7b7cc8dab56db63c6188262548b0365458a442b0
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/comet-sepolicy.mk
+++ sepolicy/comet-sepolicy.mk
@@ -4,3 +4,6 @@ BOARD_SEPOLICY_DIRS += device/google/comet/sepolicy/vendor
 # system_ext
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/comet/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/comet/sepolicy/system_ext/private
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
