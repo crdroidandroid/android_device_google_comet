3abce7dc7a67d946d42773cfa7154052cccdc12d
Michael Bestas <mkbestas@lineageos.org>
2025-12-07 03:00:37 +0200

    comet: Shuffle BoardConfig
    Change-Id: I920841e4fbd3912cc178912300b81fe86c3147cc

--- comet/BoardConfig.mk
+++ comet/BoardConfig.mk
@@ -5,18 +5,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-TARGET_BOARD_INFO_FILE := device/google/comet/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := comet
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 390
 
 include device/google/zumapro/BoardConfig-common.mk
-include device/google/comet/sepolicy/comet-sepolicy.mk
-include device/google/comet/wifi/BoardConfig-wifi.mk
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+
+# SEPolicy
+include device/google/comet/sepolicy/comet-sepolicy.mk
+
+# WiFi
+include $(DEVICE_PATH)/wifi/BoardConfig-wifi.mk
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk
